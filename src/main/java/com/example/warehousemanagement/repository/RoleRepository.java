80accbbc679608b67755846c73cf59d31ae182a5
wuseyu <1599546654@qq.com>
2025-04-01 16:41:59 +0800

    添加roleRepository

---
+++ src/main/java/com/example/warehousemanagement/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.example.warehousemanagement.repository;
+
+import com.example.warehousemanagement.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    // 根据角色名称查找角色
+    Optional<Role> findByName(String name);
+}
