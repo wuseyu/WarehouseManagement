0a8ec73d3d1174480ae9e8ade40a297115d3d075
wuseyu <1599546654@qq.com>
2025-04-02 10:29:36 +0800

    添加inventoryControllerTest

--- src/test/java/com/example/warehousemanagement/controller/InventoryControllerTest.java
+++ src/test/java/com/example/warehousemanagement/controller/InventoryControllerTest.java
@@ -17,8 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
